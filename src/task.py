2ab082ac994cf5f80313d7fdbfae6cf50ec16f58
Natalia Khodorova <icxodnik@gmail.com>
2025-03-30 21:48:43 +0200

    integrate get_upcoming_birthdays into AddressBook

--- src/task.py
+++ src/task.py
@@ -1,6 +1,6 @@
 from collections import UserDict
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Field[T]:
 
@@ -73,6 +73,9 @@ class Record:
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
+    
+    MAX_CONG_RANGE = 7
+
     def add_record(self, record):
         self.data[record.name.value] = record
     
@@ -87,6 +90,31 @@ class AddressBook(UserDict):
         if not self.data:
             return "Немає записів"
         return "\n".join(str(record) for record in self.data.values())
+    
+    #дає список привітань на цей робочий тиждень
+    def get_upcoming_birthdays(self, today = datetime.today().date()):
+        max_range = today + timedelta(days = self.MAX_CONG_RANGE)
+        # список привітальників
+        cong_list = []
+
+        for user in self.data.values():
+            birthday = user.birthday.value.replace(year = today.year)
+            
+            # Переносимо день народження тих хто народився в субботу
+            if birthday.weekday() == 5:
+                birthday = birthday + timedelta(days = 2)
+
+            # Переносимо день народження тих хто народився в неділю
+            if birthday.weekday() == 6:
+                birthday = birthday + timedelta(days = 1)
+
+            # Відсіюємо дні народження лише після врахування коректування дня вітання
+            if birthday < today or birthday > max_range:
+                continue
+
+            cong_list.append({"name": user.name.value, "congratulation_date": birthday.strftime("%Y.%m.%d")})
+
+        return cong_list
 
 if __name__ == "__main__":
 # Створення нової адресної книги
@@ -96,13 +124,14 @@ if __name__ == "__main__":
     john_record = Record("John")
     john_record.add_phone("1234567890")
     john_record.add_phone("5555555555")
-
+    john_record.add_birthday("05.04.2000")
     # Додавання запису John до адресної книги
     book.add_record(john_record)
 
     # Створення та додавання нового запису для Jane
     jane_record = Record("Jane")
     jane_record.add_phone("9876543210")
+    jane_record.add_birthday("29.03.2000")
     book.add_record(jane_record)
 
     # Виведення всіх записів у книзі
@@ -118,6 +147,13 @@ if __name__ == "__main__":
     # Пошук конкретного телефону у записі John
     found_phone = john.find_phone("5555555555")
     print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+    
+    # Виведення списку привітань на цей робочий тиждень
+    upcoming_birthdays = book.get_upcoming_birthdays(datetime(2025, 3, 27))
+    
+    print(f"Список привітань на цей робочий тиждень:")
+    for user in upcoming_birthdays:
+        print(f"{user['name']} - {user['congratulation_date']}")
 
     # Видалення запису Jane
     book.delete("Jane")
